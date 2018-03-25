fcf4c5488b48c08f3138cd5c52074a9fd23a1721
danurwijayanto <danurwijayanto@gmail.com>
2018-03-26 00:11:50 +0700

    fix_floyd_algoritm

---
+++ app/Plugins/FloydWarshall.php
@@ -0,0 +1,373 @@
+<?php
+ 
+/**
+* @package  FloydWarshall
+* @author  Janne Mikkonen 
+* @date  $Date: 2011/06/13 05:10:48 $
+* @version  $Revision: 1.1 $
+* @license  GNU General Public License, version 2 http://www.opensource.org/licenses/GPL-2.0
+**/
+
+namespace App\Plugins;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Model\PlaceCode;
+use App\Model\PlaceDetails;
+use App\Model\CodeDetails;
+
+error_reporting(E_STRICT);
+define('INFINITE', pow(2, (20 * 8 - 2)-1));
+ 
+class FloydWarshall {
+ 
+    /**
+    * Distances array
+    * @var  array
+    */
+    private $dist = array(array());
+    /**
+    * Predecessor array
+    * @var  array
+    */
+    private $pred = array(array());
+    /**
+    * Weights array
+    * @var  array
+    */
+    private $weights;
+    /**
+    * Number of nodes
+    * @var  integer
+    */
+    private $nodes;
+    /**
+    * Node names array
+    * @var  array
+    */
+    private $nodenames;
+    /**
+    * Temporary table for various stuff.
+    * @var  array
+    */
+    private $tmp = array();
+ 
+    /**
+    * Constructor
+    * @param  array $graph Graph matrice.
+    * @param  array $nodenames Node names as an array.
+    */
+    public function __construct($graph, $nodenames='') {
+ 
+        $this->weights = $graph;
+        $this->nodes   = count($this->weights);
+        if ( ! empty($nodenames) && $this->nodes == count($nodenames) ) {
+            $this->nodenames = $nodenames;
+        }
+        $this->__floydwarshall();
+ 
+    }
+ 
+    /**
+    * The actual PHP implementation of Floyd-Warshall algorithm.
+    * @return  void
+    */
+    private function __floydwarshall () {
+ 
+        // Initialization
+        for ( $i = 0; $i < $this->nodes; $i++ ) {
+            for ( $j = 0; $j < $this->nodes; $j++ ) {
+                if ( $i == $j ) {
+                    $this->dist[$i][$j] = 0;
+                } else if ( $this->weights[$i][$j] > 0 ) {
+                    $this->dist[$i][$j] = $this->weights[$i][$j];
+                } else {
+                    $this->dist[$i][$j] = INFINITE;
+                }
+                $this->pred[$i][$j] = $i;
+            }
+        }
+ 
+        // Algorithm
+ 
+        for ( $k = 0; $k < $this->nodes; $k++ ) {
+            for ( $i = 0; $i < $this->nodes; $i++ ) {
+                for ( $j = 0; $j < $this->nodes; $j++ ) {
+                    if ($this->dist[$i][$j] > ($this->dist[$i][$k] + $this->dist[$k][$j])) {
+                        $this->dist[$i][$j] = $this->dist[$i][$k] + $this->dist[$k][$j];
+                        $this->pred[$i][$j] = $this->pred[$k][$j]; 
+                        //echo "<br>".$this->pred[$k][$j];
+                    }
+                }
+            }
+        }
+ 
+    }
+ 
+    /**
+    * Private method to get the path.
+    *
+    * Get graph path from predecessor matrice.
+    * @param  integer $i
+    * @param  integer $j
+    * @return  void
+    */
+    private function __get_path($i, $j) {
+ 
+        if ( $i != $j ) {
+            $this->__get_path($i, $this->pred[$i][$j]);
+        }
+        array_push($this->tmp, $j);
+    }
+ 
+    /**
+    * Public function to access get path information.
+    *
+    * @param  ingeger $i Starting node.
+    * @param  integer $j End node.
+    * @return  array Return array of nodes.
+    */
+    public function get_path($i, $j) {
+        $this->__get_path($i, $j);
+        return $this->tmp;
+    }
+ 
+    /**
+    * Print nodes from a and b.
+    * @param  ingeger $i Starting node.
+    * @param  integer $j End node.
+    * @return  void
+    */
+    public function print_path($i, $j) {
+ 
+        if ( $i != $j ) {
+            $this->print_path($i, $this->pred[$i][$j]);
+        }
+ 
+        if (! empty($this->nodenames) ) {
+            print($this->nodenames[$j]) . ' ';
+        } else {
+            print($j) . ' ';
+        }
+ 
+    }
+ 
+    /**
+    * Get total cost (distance) between point a to b.
+    *
+    * @param  integer $i
+    * @param  ingeger $j
+    * @return  array Returns an array of costs.
+    */
+    public function get_distance($i, $j) {
+        return $this->dist[$i][$j];
+    }
+ 
+    /************************************************************
+    ***                    DEBUG FUNCTIONS                    ***
+    ***                    - print_graph                      ***
+    ***                    - print_dist                       ***
+    ***                    - print_pred                       ***
+    *************************************************************/
+ 
+    /**
+    * Print out the original Graph matrice.
+    * @return  void
+    */
+    public function print_graph () {
+ 
+ 
+        //if ( empty($_SERVER['argv']) ) {
+            echo '<strong>Graph</strong><br>';
+            echo '';
+            if (! empty($this->nodenames) ) {
+                echo '';
+                echo '';
+                for ($n = 0; $n < $this->nodes; $n++) {
+                    echo '';
+                }
+            }
+            echo '';
+            for ($i = 0; $i < $this->nodes; $i++) {
+                echo '';
+                if (! empty($this->nodenames) ) {
+                    echo '';
+                }
+                for ($j = 0; $j < $this->nodes; $j++) {
+                    echo '';
+                }   
+                echo '';
+            }
+            $print_table = '<table border="1" cellpadding="4" width="100%">';
+            $print_table .= '<tbody>';
+            $print_table .= '<tr><td></td>';
+            
+            for ($i = 0; $i < $this->nodes; $i++) {
+                $print_table .= '<td align="center">
+                                    <strong>' .$this->nodenames[$i] .'</strong>
+                                </td>';
+            };
+            $print_table .= '<tr>';
+            for ($i = 0; $i < $this->nodes; $i++) {
+                $print_table .= '<tr><td>'.$this->nodenames[$i].'</td>';
+                for ($j = 0; $j < $this->nodes; $j++) {
+                    $print_table .= '<td  align="center"><strong>' .
+                                        $this->weights[$i][$j] .
+                                        '</strong>
+                                    </td>';
+                }
+                $print_table .= '</tr>';
+            }
+            $print_table .= '</tbody>';
+            $print_table .= '</table>';
+                        
+            // '<tr>
+            //                 <td> </td>
+            //                 <td width="15" align="center">
+            //                     <strong>' .$this->nodenames[0] .'</strong>
+            //                 </td>
+            //             </tr>
+            //             <tr>
+            //                 <td width="15" align="center"><strong>' .
+            //                     $this->nodenames[0] .
+            //                     '</strong>
+            //                 </td>
+            //                 <td width="15" align="center">' .
+            //                     $this->weights[0][1] . 
+            //                 '</td>
+            //             </tr>
+            //         </tbody>
+            //     </table><br>';
+            echo $print_table;
+       // } else {
+ 
+        //}
+    }
+ 
+    /**
+    * Print out distances matrice.
+    * @return  void
+    */
+    public function print_dist () {
+ 
+        //if ( empty($_SERVER['argv']) ) {
+            echo '<strong>Distances</strong><br>';
+            echo '';
+            if (! empty($this->nodenames) ) {
+                echo '';
+                echo '';
+                for ($n = 0; $n < $this->nodes; $n++) {
+                    echo '';
+                }
+            }
+            echo '';
+            for ($i = 0; $i < $this->nodes; $i++) {
+                echo '';
+                if (! empty($this->nodenames) ) {
+                    echo '';
+                }
+                for ($j = 0; $j < $this->nodes; $j++) {
+                    echo '';
+                }
+                echo '';
+            }
+            // echo '<table border="1" cellpadding="4" width="100%"><tbody><tr><td> </td><td width="15" align="center"><strong>' .
+            //             $this->nodenames[$n] .
+            //             '</strong></td></tr><tr><td width="15" align="center"><strong>' .
+            //              $this->nodenames[$i] .
+            //             '</strong></td><td width="15" align="center">' .
+            //              $this->dist[$i][$j] . '</td></tr></tbody></table><br>';
+
+            $print_table = '<table border="1" cellpadding="4" width="100%">';
+            $print_table .= '<tbody>';
+            $print_table .= '<tr><td></td>';
+            
+            for ($i = 0; $i < $this->nodes; $i++) {
+                $print_table .= '<td align="center">
+                                    <strong>' .$this->nodenames[$i] .'</strong>
+                                </td>';
+            };
+            $print_table .= '<tr>';
+            for ($i = 0; $i < $this->nodes; $i++) {
+                $print_table .= '<tr><td>'.$this->nodenames[$i].'</td>';
+                for ($j = 0; $j < $this->nodes; $j++) {
+                    $print_table .= '<td  align="center"><strong>' .
+                                        $this->dist[$i][$j] .
+                                        '</strong>
+                                    </td>';
+                }
+                $print_table .= '</tr>';
+            }
+            $print_table .= '</tbody>';
+            $print_table .= '</table>';
+            echo $print_table;
+        //} else {
+        //    echo "cmd line not yet completed!\n";
+        //}
+ 
+    }
+ 
+    /**
+    * Print out predecessors matrice.
+    * @return  void
+    */
+    public function print_pred () {
+ 
+        //if ( empty($_SERVER['argv']) ) {
+            echo '<strong>Predecessors</strong><br>';
+            echo '';
+            if (! empty($this->nodenames) ) {
+                echo '';
+                echo '';
+                for ($n = 0; $n < $this->nodes; $n++) {
+                    echo '';
+                }
+            }
+            echo '';
+            for ($i = 0; $i < $this->nodes; $i++) {
+                echo '';
+                if (! empty($this->nodenames) ) {
+                    echo '';
+                }
+                for ($j = 0; $j < $this->nodes; $j++) {
+                    echo '';
+                }
+                echo '';
+            }
+            // echo '<table border="1" cellpadding="4" width="100%"><tbody><tr><td> </td><td width="15" align="center"><strong>' .
+            //              $this->nodenames[$n] .
+            //             '</strong></td></tr><tr><td width="15" align="center"><strong>' .
+            //              $this->nodenames[$i] .
+            //             '</strong></td><td width="15" align="center">' .
+            //         $this->pred[$i][$j] . '</td></tr></tbody></table><br>';
+            $print_table = '<table border="1" cellpadding="4" width="100%">';
+            $print_table .= '<tbody>';
+            $print_table .= '<tr><td></td>';
+            
+            for ($i = 0; $i < $this->nodes; $i++) {
+                $print_table .= '<td align="center">
+                                    <strong>' .$this->nodenames[$i] .'</strong>
+                                </td>';
+            };
+            $print_table .= '<tr>';
+            for ($i = 0; $i < $this->nodes; $i++) {
+                $print_table .= '<tr><td>'.$this->nodenames[$i].'</td>';
+                for ($j = 0; $j < $this->nodes; $j++) {
+                    $print_table .= '<td  align="center"><strong>' .
+                                        $this->pred[$i][$j] .
+                                        '</strong>
+                                    </td>';
+                }
+                $print_table .= '</tr>';
+            }
+            $print_table .= '</tbody>';
+            $print_table .= '</table>';
+            echo $print_table;
+       // } else {
+        //    echo "cmd line not yet completed!\n";
+        //}
+ 
+    }
+ 
+} // End of class
+?>
--- app/Traits/Algoritm.php
+++ app/Traits/Algoritm.php
@@ -8,28 +8,93 @@ use App\Model\PlaceCode;
 use App\Model\PlaceDetails;
 use App\Model\CodeDetails;
 use RFHaversini\Distance;
+use App\Plugins\FloydWarshall;
 
 trait Algoritm{
     public function getDistance(Request $request){
         // Define variabel
         $from = $request->dari;
         $destination = $request->tujuan;
+        $from = PlaceDetails::find($from);
+        $destination = PlaceDetails::find($destination);
+        $allDestination = PlaceDetails::get();
+        /** GMAPS */
+        /*
         $url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial";
         $origins = "&origins=-7.074161,110.432116";
         $destination = "&destinations=-6.981927,110.366058";
         $key = "&key=".env("GMAPS_TOKEN");
-
-        
-        // $from = PlaceDetails::find($from);
-        // $destination = PlaceDetails::find($destination);
-        // $allDestination = PlaceDetails::get();
-
         $client = new \GuzzleHttp\Client();
         $res = $client->request('GET', $url.$origins.$destination.$key);
-        // dd($res->getStatusCode());
-        // dd($res->getHeaderLine('content-type'));
-        $inKilometers = Distance::toKilometers(-7.074161, 110.432116, -6.981927, 110.366058);
+        dd($res->getStatusCode());
+        dd($res->getHeaderLine('content-type'));
+        */
+        
+        if (!empty($allDestination)){
+            $index = count($allDestination);
+            $node = array();
+            $node_id = array();
+            $distance_matrix[] = array();
+            $calculate_distance = 0;
+            
+            foreach($allDestination as $destination){
+                array_push($node, $destination->pd_name);
+                array_push($node_id, $destination->pd_id);
+            }
+
+            for ($i=0; $i<$index; $i++){
+                for ($j=0; $j<$index; $j++){
+                    if ($i == $j){
+                        $calculate_distance = 0;
+                    }else{
+                        $calculate_distance = Distance::toKilometers($allDestination[$i]->pd_latitude, $allDestination[$i]->pd_longitude, $allDestination[$j]->pd_latitude, $allDestination[$j]->pd_longitude);
+                    }
+                    $distance_matrix[$i][$j] = $calculate_distance;
+                }
+            }
+            // $inKilometers = Distance::toKilometers(-7.074161, 110.432116, -6.981927, 110.366058);
+            // dd($node_id);
+        }
+
         // $inMiles      = Distance::toMiles(30.261699, -97.738967, 29.869229, -97.959595);
-        dd(json_decode($inKilometers));
-    }
+        //dd(json_decode($inKilometers));
+
+        /** Floyd Warshall */
+        $graph = array(array(0,5,8,6),
+                      array(5,0,3,1),
+                      array(8,3,0,1),
+                      array(6,1,1,0));
+                    //   array(0,0,10,0,0,4),
+                    //   array(0,0,17,20,0,0));
+        $nodes = array("a", "b", "c", "d");
+        // dd($nodes);
+        $fw = new FloydWarshall($graph, $nodes);
+        // dd($fw);
+        // $fw->print_path(0,3);
+        $fw->print_graph();
+        // $fw->print_dist();
+        // $fw->print_pred();
+    
+        $sp = $fw->get_path(0,3);
+        // dd($sp);
+        $jlss = 3;
+        echo 'Ruter Terdekat Dari Star Ke '.$nodes[$jlss].' Adalah: <strong>';
+        $jl = count($sp);
+        $r=1;
+        foreach ($sp as $value) {
+            echo $nodes[$value];
+            if ($r == $jl) {
+                
+            }
+            else{
+                echo ' => ';
+            }
+            $r++;
+                
+        }
+        echo ' | Dengan Jarak Tempuh ';
+        $menit = round(($fw->get_distance(0,$jlss)/40)*60,0);
+        print_r($fw->get_distance(0,$jlss));
+        echo " km yaitu $menit</strong>";
+        }
 }
